fc5283eed161d2fda3a9ba3028417ded4d090a1c
Carlos Costa <carlos.adriano@ecommet.com.br>
2018-08-15 22:51:03 -0300

    feat: Criando sistema de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ROUTES } from './app.routes';
+import { FornecedorComponent } from './fornecedor/fornecedor.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, FornecedorComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES, { useHash: true }),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+
+import { FornecedorComponent } from './fornecedor/fornecedor.component';
+
+export const ROUTES: Routes = [{ path: '', component: FornecedorComponent }];
